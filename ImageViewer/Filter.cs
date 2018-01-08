2295aac9ebf6ed6db0188b83960cc9716f1dc5c6
Andrei <andrei.saharoff3011@gmail.com>
2018-01-08 14:27:52 +0300

    Add files via upload

---
+++ ImageViewer/Filter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageViewer
+{
+    class Filter
+    {
+        public Bitmap Grayscale (Bitmap image)
+        {
+            int rgb;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    var currentPixel = image.GetPixel(i, j);
+                    rgb = Convert.ToInt32(0.3 * currentPixel.R + 0.587 * currentPixel.G + 0.114 * currentPixel.B);
+                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+            return image;
+        }
+
+        public Bitmap Sepia (Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    var currentPixel = image.GetPixel(i, j);
+                    int r = Convert.ToInt32(0.393 * currentPixel.R + 0.769 * currentPixel.G + 0.189 * currentPixel.B);
+                    int g = Convert.ToInt32(0.349 * currentPixel.R + 0.686 * currentPixel.G + 0.168 * currentPixel.B);
+                    int b = Convert.ToInt32(0.272 * currentPixel.R + 0.534 * currentPixel.G + 0.131 * currentPixel.B);
+                    r = r > 255 ? 255 : r;
+                    g = g > 255 ? 255 : g;
+                    b = g > 255 ? 255 : b;
+                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(r, g, b));
+                }
+            }
+            return image;
+        }
+
+        public Bitmap Pink (Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    var currentPixel = image.GetPixel(i, j);
+                    int r = Convert.ToInt32(0.593 * currentPixel.R + 0.569 * currentPixel.G + 0.189 * currentPixel.B);
+                    r = r > 255 ? 255 : r;
+                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(r, currentPixel.G, currentPixel.B));
+                }
+            }
+            return image;
+        }
+
+        public Bitmap Greeny(Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    var currentPixel = image.GetPixel(i, j);
+                    int g = Convert.ToInt32(0.349 * currentPixel.R + 0.686 * currentPixel.G + 0.168 * currentPixel.B);
+                    g = g > 255 ? 255 : g;
+                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(currentPixel.R, g, currentPixel.B));
+                }
+            }
+            return image;
+        }
+
+        public Bitmap Yellow(Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    var currentPixel = image.GetPixel(i, j);
+                    int r = Convert.ToInt32(0.7 * currentPixel.R + 0.45 * currentPixel.G + 0.1 * currentPixel.B);
+                    int g = Convert.ToInt32(0.5 * currentPixel.R + 0.65 * currentPixel.G + 0.1 * currentPixel.B);
+                    r = r > 255 ? 255 : r;
+                    g = g > 255 ? 255 : g;
+                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(r, g, currentPixel.B));
+                }
+            }
+            return image;
+        }
+
+        public Bitmap Violet(Bitmap image)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    var currentPixel = image.GetPixel(i, j);
+                    int r = Convert.ToInt32(0.45 * currentPixel.R + 0.1 * currentPixel.G + 0.5 * currentPixel.B);
+                    int b = Convert.ToInt32(0.4 * currentPixel.R + 0.2 * currentPixel.G + 0.8 * currentPixel.B);
+                    r = r > 255 ? 255 : r;
+                    b = b > 255 ? 255 : b;
+                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(r, currentPixel.G, b));
+                }
+            }
+            return image;
+        }
+    }
+}
---
+++ ImageViewer/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Microsoft.Win32;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            infoHeader.Height = ActualHeight / 6;
+            editHeader.Height = infoHeader.Height;
+            rotateHeader.Height = infoHeader.Height;
+            filterHeader.Height = infoHeader.Height;
+            infoHeader.Height = infoHeader.Height;
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog
+            {
+                Title = "Select a picture",
+                Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+            "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+            "Portable Network Graphic (*.png)|*.png",
+            };
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "Document";
+            dlg.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|PNG Image|*.png";
+            if (dlg.ShowDialog() == true)
+            {
+                var encoder = new PngBitmapEncoder(); //
+                encoder.Frames.Add(BitmapFrame.Create((BitmapImage)imgPhoto.Source));
+                using (var stream = dlg.OpenFile())
+                {
+                    encoder.Save(stream);
+                }
+            }
+        }
+
+        private void RotateClick(object sender, RoutedEventArgs e)
+        {
+            TransformedBitmap tb = new TransformedBitmap();
+            tb.BeginInit();
+            try
+            {
+                var tmpImage = (BitmapImage)imgPhoto.Source;
+                tb.Source = tmpImage;
+            }
+            catch
+            {
+                tb.Source = (TransformedBitmap)imgPhoto.Source;
+            }
+            RotateTransform transform = new RotateTransform(90);
+            tb.Transform = transform;
+            tb.EndInit();
+            imgPhoto.Source = tb;
+        }
+
+        private void GreyscaleClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            Filter filter = new Filter();
+            imageBmp = filter.Grayscale(imageBmp);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void SepiaClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            Filter filter = new Filter();
+            imageBmp = filter.Sepia(imageBmp);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void PinkClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            Filter filter = new Filter();
+            imageBmp = filter.Pink(imageBmp);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void GreenyClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            Filter filter = new Filter();
+            imageBmp = filter.Greeny(imageBmp);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void YellowClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            Filter filter = new Filter();
+            imageBmp = filter.Yellow(imageBmp);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void VioletClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            Filter filter = new Filter();
+            imageBmp = filter.Violet(imageBmp);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void BlurClick(object sender, RoutedEventArgs e)
+        {
+            BitmapSource bitmap = (BitmapSource)imgPhoto.Source;
+            Bitmap imageBmp = BitmapImage2Bitmap(bitmap);
+            GaussianBlur blur = new GaussianBlur(imageBmp);
+            imageBmp = blur.Process(5);
+            imgPhoto.Source = BitmapToBitmapImage(imageBmp);
+        }
+
+        private void InfoClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Width: " + imgPhoto.Source.Width + ", Height: " + imgPhoto.Source.Height);
+        }
+
+        private static Bitmap BitmapImage2Bitmap(BitmapSource bitmapImage)
+        {
+            using (var outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                var bitmap = new Bitmap(outStream);
+                return bitmap;
+            }
+        }
+
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
